890ea927a82636aff2e7b280b3223b96a072a7fa
ST-18 <alexgb@pacificu.edu>
2015-05-18 21:03:00 -0700

    push Hellow_World project

---
+++ Hello_World/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ * A simple "Hello World" project to test basic environment set up.
+ */
+public class HelloWorld {
+	
+	public static void main (String [] args) {
+		System.out.print("Hello World!");
+	}
+}
